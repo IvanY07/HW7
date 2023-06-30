c5d1687fb478a7bc41e1e16704c775ab69ef538b
IvanY <ivan.yavtushenko@bk.ru>
2023-06-30 14:16:42 +0300

    Написали программу для увольнения сотрудников

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int itIdCount = 100;
+        int Id = 2;
+//        for (int i = 2; i <= itIdCount; i+=2) {
+//            System.out.println("Уволен айтишник с id " + i);
+//        }
+        while (Id <= itIdCount) {
+            System.out.println("Уволен айтишник с id " + Id);
+            Id += 2;
+        }
     }
 }
